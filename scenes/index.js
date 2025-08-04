e2a48a69514e48ed55d13c5870d9d44e2f3a9d9e
profedani1 <danielmafegomez@gmail.com>
2025-08-04 14:17:31 -0300

    Delete scenes/index.js

--- scenes/index.js
+++
@@ -1,7 +0,0 @@
-// scenes/index.js
-export const scenes = [
-  () => import('./esfera.js'),
-  () => import('./cubo.js'),
-  // Aquí añades nuevas escenas sin tocar main.js
-  // () => import('./otraEscena.js'),
-];
